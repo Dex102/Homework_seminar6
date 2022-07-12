aa4ac85e7a59f7b1bb07f2036e30130d3eb84251
EgorEvtushenko <ljvbyj0990@gmail.com>
2022-07-12 16:50:04 +0500

    Homework semiinar 6

---
+++ z1/Program.cs
@@ -0,0 +1,40 @@
+﻿int CountPositive (int[] array)
+{
+    int count = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] MyArray = new int[size];
+
+for(int i = 0; i < size; i++)
+{
+    Console.Write("Input a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    MyArray[i] = number;
+}
+
+ShowArray(MyArray);
+
+Console.Write("Count positive numbers is: " + CountPositive(MyArray));
+
---
+++ z2/Program.cs
@@ -0,0 +1,25 @@
+﻿void IntersectionPoint (double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+    Console.Write($"IntersectionPoint is ({x}; {y})");
+}
+
+Console.Write("Input b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 != k2)
+{
+    IntersectionPoint(b1, k1, b2, k2);
+}
+
+else if (k1 == k2)
+{
+    Console.Write("ERROR! k1 = k2! Input new!");
+}
